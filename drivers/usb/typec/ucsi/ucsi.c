a3a8a31d16715f8e31407270e4ea086cf0e92b5b
Linyu Yuan <quic_linyyuan@quicinc.com>
2022-07-26 14:45:49 +0800

    usb: typec: ucsi: Acknowledge the GET_ERROR_STATUS command completion
    commit 4edb124a5116be49d757ce2775dc4cfc023c5926 from
    https://nv-tegra.nvidia.com/r/3rdparty/canonical/linux-jammy.git
    l4t/l4t-r36.2-Ubuntu-nvidia-tegra-1018.18
    
    BugLink: https://bugs.launchpad.net/bugs/1990162
    
    commit a7dc438b5e446afcd1b3b6651da28271400722f2 upstream.
    
    We found PPM will not send any notification after it report error status
    and OPM issue GET_ERROR_STATUS command to read the details about error.
    
    According UCSI spec, PPM may clear the Error Status Data after the OPM
    has acknowledged the command completion.
    
    This change add operation to acknowledge the command completion from PPM.
    
    Fixes: bdc62f2bae8f (usb: typec: ucsi: Simplified registration and I/O API)
    Cc: <stable@vger.kernel.org> # 5.10
    Signed-off-by: Jack Pham <quic_jackp@quicinc.com>
    Signed-off-by: Linyu Yuan <quic_linyyuan@quicinc.com>
    Link: https://lore.kernel.org/r/1658817949-4632-1-git-send-email-quic_linyyuan@quicinc.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Rajeshkumar Ramasamy <rajeshkumar.ramasamy@windriver.com>

--- drivers/usb/typec/ucsi/ucsi.c
+++ drivers/usb/typec/ucsi/ucsi.c
@@ -80,6 +80,10 @@ static int ucsi_read_error(struct ucsi *ucsi)
 	if (ret)
 		return ret;
 
+	ret = ucsi_acknowledge_command(ucsi);
+	if (ret)
+		return ret;
+
 	switch (error) {
 	case UCSI_ERROR_INCOMPATIBLE_PARTNER:
 		return -EOPNOTSUPP;
